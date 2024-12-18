a1b2579d6f589daea1988581eb56f92a417d776f
zhangYunPeng <2250079463@qq.com>
2024-12-18 20:03:52 +0800

    引入typeorm

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 
 ## 微信小程序服务
 
-## 初始化项目
+## 下载依赖包
 ```bash
 $ npm install
+```
+
+## 本地运行
+```bash
+$ npm run build
+```
+
+## 项目构建
+```bash
+$ npm run build
+```
+
+## nest-cli.json文件配置 
+```
+  "generateOptions": {
+    "spec": false //生成代码的时候不会生成测试代码
+   },
 ```
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test', //数据库名称
+      synchronize: true, //自动同步数据库结构
+      logging: true,
+      entities: [], //指定数据库实体
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+          authPlugin: 'sha256_password',
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
