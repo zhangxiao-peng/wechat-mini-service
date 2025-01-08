e80df9c687c7a85cf540525a9c04730f49dd102a
zhangYunPeng <2250079463@qq.com>
2025-01-09 00:15:09 +0800

    修改数据库和redis配置,环境配置文件修改

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RedisModule } from './redis/redis.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -11,21 +11,26 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: 'src/.env'
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test', //数据库名称
-      synchronize: true, //自动同步数据库结构
-      logging: true,
-      entities: [], //指定数据库实体
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugin: 'sha256_password',
-      }
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return{
+          type: 'mysql',
+          host: configService.get('mysql_server_host'),
+          port: configService.get('mysql_server_port'),
+          username: configService.get('mysql_server_username'),
+          password: configService.get('mysql_server_password'),
+          database: configService.get('mysql_server_database'),
+          synchronize: true,
+          logging: true,
+          entities: [],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          extra: {
+            authPlugin: 'sha256_password',
+          }
+        }
+      },
+      inject:[ConfigService]
     }),
     RedisModule
   ],
